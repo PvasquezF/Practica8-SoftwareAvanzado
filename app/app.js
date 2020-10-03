152b9e5380782cc08276d36aef49dc61fbfc6aa4
Pavel Vasquez <alexanderpavelv32@gmail.com>
2020-10-03 05:54:55 -0600

    Subiendo nuevos cambios

--- app/app.js
+++ app/app.js
@@ -3,14 +3,20 @@ const app = express()
 const port = 3000;
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'http://35.202.209.170',
+    host: 'localhost',
     port: 3306,
-    user: 'root',
+    user: 'sa',
     password: '12345',
     database: 'practica8-softwareavanzado'
 });
 
 app.get('/', (req, res) => {
+    res.status(200).json({
+        result: 'hola'
+    });
+});
+
+app.get('/db', (req, res) => {
     connection.connect();
 
     connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
