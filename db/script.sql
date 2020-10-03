3ecd25043671219e67e107fbf5dcaa3302cc5797
Pavel Vasquez <alexanderpavelv32@gmail.com>
2020-10-02 23:12:39 -0600

    Subiendo nuevos cambios

---
+++ db/script.sql
@@ -0,0 +1,8 @@
+CREATE TABLE animales(
+    id integer primary key autoincrement,
+    nombre varchar(250),
+    foto varchar(250)
+);
+
+INSERT INTO TABLE animales VALUES('Gato', 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg');
+INSERT INTO TABLE animales VALUES('Perro', 'https://www.hola.com/imagenes/estar-bien/20190820147813/razas-perros-pequenos-parecen-grandes/0-711-550/razas-perro-pequenos-grandes-a.jpg?filter=ds75');
